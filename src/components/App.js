ea3fc3fd4ebd7f7b8eb75e1177524777dcc8dd46
Barbara Luz <barbaraluz@eq.ufrj.br>
2022-09-26 03:45:53 -0300

    lógica pronta

--- src/components/App.js
+++ src/components/App.js
@@ -8,36 +8,24 @@ function Imagem(props) {
     return <img src={`assets/forca${props.erros}.png`} />
 }
 
-function Letra(props) {
-    return <LetraStyle><p>{props.letra.toUpperCase()}</p></LetraStyle>
-}
-
-function Chute() {
-    return (
-        <span>
-            <span>Já sei a palavra!</span>
-            <input></input>
-            <button>Chutar</button>
-        </span>
-    )
-}
-
 export default function App() {
 
     const [erros, setErros] = React.useState(0)
     const [palavraEscolhida, setPalavraEscolhida] = React.useState([])
     const [palavraEscondida, setPalavraEscondida] = React.useState([])
     const [letrasEscolhidas, setLetrasEscolhidas] = React.useState([])
+    const [palavraChutada, setPalavraChutada] = React.useState([])
 
     function escolherPalavra() {
+        setErros(0)
+        setLetrasEscolhidas([])
         const palavra = palavras[Math.floor(Math.random() * 231)].split('')
         setPalavraEscolhida(palavra)
-        setPalavraEscondida(palavra.map(l => '_ '))
+        setPalavraEscondida(palavra.map(l => ' _ '))
     }
 
-    function escolherLetra(l, palavraEscolhida) {
+    function escolherLetra(l) {
         setLetrasEscolhidas([...letrasEscolhidas, l])
-        console.log(palavraEscolhida)
         if (palavraEscolhida.includes(l)) {
             let palavra = [...palavraEscondida]
             for (let i=0; i<palavraEscolhida.length; i++) {
@@ -48,6 +36,18 @@ export default function App() {
             }
         } else {
             setErros(erros + 1)
+            if (erros + 1 === 6) {
+                setPalavraEscondida(palavraEscolhida)
+            }
+        }
+    }
+
+    function checaPalavraChutada() {
+        if (palavraEscolhida.join() === palavraChutada.join()) {
+            setPalavraEscondida(palavraChutada)
+        } else {
+            setErros(6)
+            setPalavraEscondida(palavraEscolhida)
         }
     }
 
@@ -56,16 +56,20 @@ export default function App() {
             <GlobalStyle />
             <Visual>
                 <Imagem erros={erros} />
-                <Palavra>
-                    <Escolha onClick={escolherPalavra}><p>Escolher Palavra</p></Escolha>
-                    {palavraEscondida.length > 0 ? palavraEscondida : ''}
+                <Palavra palavraEscolhida={palavraEscolhida} palavraEscondida={palavraEscondida} erros={erros}>
+                    <Escolha onClick={escolherPalavra}>Escolher Palavra</Escolha>
+                    {palavraEscondida.length > 0 ? <p>{palavraEscondida}</p> : ''}
                 </Palavra>
             </Visual>
             <Acertos>
                 <Alfabeto>
-                    {alfabeto.map(l => <LetraStyle onClick={() => escolherLetra(l, palavraEscolhida)} palavraEscolhida={palavraEscolhida}><p>{l.toUpperCase()}</p></LetraStyle>)}
+                    {alfabeto.map(l => <LetraStyle disabled={letrasEscolhidas.includes(l) || palavraEscolhida.length === 0 || erros === 6 || (palavraEscolhida.join() === palavraEscondida.join() && erros < 6) ? true : false} onClick={() => escolherLetra(l, palavraEscolhida)} palavraEscolhida={palavraEscolhida}><p>{l.toUpperCase()}</p></LetraStyle>)}
                 </Alfabeto>
-                <Chute />
+                <Chute>
+                    <span>Já sei a palavra!</span>
+                    <input disabled={palavraEscolhida.length === 0 || erros === 6 || (palavraEscolhida.join() === palavraEscondida.join() && erros < 6) ? true : false} onChange={e => setPalavraChutada(e.target.value.split(''))}/>
+                    <button onClick={checaPalavraChutada}>Chutar</button>
+                </Chute>
             </Acertos>
         </Wrapper>
     )
@@ -74,7 +78,6 @@ export default function App() {
 const Wrapper = styled.div`
 display: flex;
 flex-direction: column;
-background-color: red;
 
 img {
     width: 500px;
@@ -85,7 +88,6 @@ const Visual = styled.div`
 display: flex;
 justify-content: center;
 gap: 150px;
-background-color: blue;
 margin-bottom: 20px
 `
 
@@ -98,30 +100,35 @@ border: none;
 border-radius: 10px;
 margin-top: 50px;
 cursor: pointer;
-
-p {
-    font-size: 18px;
-    font-weight: 700
-}
+font-size: 18px;
+font-weight: 700
 `
 
 const Palavra = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: space-between;
+padding-bottom: 20px;
+color: ${props => (props.palavraEscolhida.join() === props.palavraEscondida.join() && props.erros < 6) ? '#27AE60' : props.erros === 6 ? 'red' : 'black'};
+font-weight: 700;
+
+p {
+    font-size: 50px;
+}
 `
 
 const Acertos = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
+gap: 25px;
 `
 
 const Alfabeto = styled.div`
 display: flex;
 width: 750px;
 flex-wrap: wrap;
-gap: 5px
+gap: 5px;
 `
 
 const LetraStyle = styled.button`
@@ -130,16 +137,37 @@ height: 50px;
 display: flex;
 justify-content: center;
 align-items: center;
-background-color: #E1ECF4;
-color: #39739D;
-border: 1px solid #7CA8C8;
+background-color: ${props => props.disabled ? '#9FAAB5' : '#E1ECF4'};
+color: ${props => props.disabled ? '#7B838D' : '#39739D'};
+border: 1px solid ${props => props.disabled ? '#9FAAB5' : '#7CA8C8'};
 border-radius: 5px;
 cursor: ${props => props.palavraEscolhida.length > 0 ? 'pointer' : ''};
-
-&:disabled: ${props => props.palavraEscolhida.length > 0 ? false : true};
 
 p {
     font-weight: 700;
     font-size: 16px
+}
+`
+
+const Chute = styled.div`
+display: flex;
+align-items: center;
+gap: 10px;
+font-size: 20px;
+
+input {
+    width: 400px;
+    height: 27px;
+    border-radius: 4px;
+}
+
+button {
+    height: 37px;
+    width: 70px;
+    background-color: #E1ECF4;
+    color: #39739D;
+    border: 1px solid #7CA8C8;
+    border-radius: 5px;
+    cursor: pointer;
 }
 `
